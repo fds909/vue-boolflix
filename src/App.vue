b4f1af8a42146d2f4ce0a6b934da176b5b0bb482
Francesco De Sìmini <francescod995@gmail.com>
2022-04-22 17:00:00 +0200

    recuperati dati film

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <HeaderComp/>
+    <HeaderComp @search="searchTitle"/>
+    <MainComp/>
   </div>
 </template>
 
@@ -14,11 +15,47 @@ export default {
   components: {
     HeaderComp,
     MainComp
-  }
+  },
+  data() {
+    return {
+      apiKey: '480ca1b608920c32ceb1d868578f7cdf',
+      searchText: '',
+      movies: [],
+      series: []
+    }
+  },
+  methods: {
+    searchTitle(text) {
+      this.searchText = text;
+      console.log(this.searchText);
+
+      if (this.searchText != '') {
+        axios.get( `https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.searchText}&language=it-IT`)
+        .then( (res) => {
+          console.log(res.data.results);
+          
+          res.data.results.forEach(element => {
+            let movie = {
+              'title': element.title,
+              'originalTitle': element.original_title,
+              'language': element.original_language,
+              'vote': element.vote_average
+            }
+
+            this.movies.push(movie);
+          });
+
+          console.log(this.movies);
+        });
+      }
+      
+    }
+  } 
 }
 </script>
 
 <style lang="scss">
+  // Reset
   * {
     margin: 0;
     padding: 0;
