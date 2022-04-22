a4893bcb544ff2e926404aa46bcb7da9ae3f40cf
Francesco De Sìmini <francescod995@gmail.com>
2022-04-22 15:49:18 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <HeaderComp/>
+  </div>
+</template>
+
+<script>
+import HeaderComp from './components/HeaderComp.vue';
+import MainComp from './components/MainComp.vue'
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComp,
+    MainComp
+  }
+}
+</script>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
